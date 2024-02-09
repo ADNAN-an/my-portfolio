c2ae29d22a9b9527d0e0da59e81373511aa2fbc7
ADNAN-an <adnan.anjar2002@gmail.com>
2024-02-09 02:53:25 +0100

    eslint fixed

--- components/intro.tsx
+++ components/intro.tsx
@@ -47,7 +47,7 @@ export default function Intro() {
            initial={{opacity: 0, y: 100}}
            animate={{opacity: 1, y: 0}}
         >
-        <span className="font-bold">Hello, I'm Adnan.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Adnane.</span> I'm a{" "}
         <span className="font-bold">Software engineering student</span> with{" "}
         <span className="font-bold">2 months</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
