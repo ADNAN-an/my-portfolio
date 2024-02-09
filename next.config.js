fcf5cc226bbd38ebdb5b26df7b00cebaa7d615af
ADNAN-an <adnan.anjar2002@gmail.com>
2024-02-09 02:48:17 +0100

    eslint fixed

--- next.config.js
+++ next.config.js
@@ -10,7 +10,10 @@ const nextConfig = {
     },
     experimental: {
         serverActions: true,
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
